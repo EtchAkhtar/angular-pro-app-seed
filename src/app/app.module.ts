e8b574c18b24cf17f3b67aaa3c87200355f90ddc
Etch Akhtar <etch@clearcutcoding.com>
2018-07-02 17:41:42 +0100

    1st deployment to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,18 +15,20 @@ import { AppComponent } from './containers/app/app.component';
 export const ROUTES: Routes = [];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES)
-  ],
-  declarations: [
-    AppComponent
-  ],
-  providers: [
-    Store
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
+  declarations: [AppComponent],
+  providers: [Store],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+/*
+var config = {
+    apiKey: "AIzaSyB21UPOlj4Bd0gbZ0zFfQyCxcuRLJBbIQQ",
+    authDomain: "ultimate-angular-fitness-d38d9.firebaseapp.com",
+    databaseURL: "https://ultimate-angular-fitness-d38d9.firebaseio.com",
+    projectId: "ultimate-angular-fitness-d38d9",
+    storageBucket: "ultimate-angular-fitness-d38d9.appspot.com",
+    messagingSenderId: "499907024865"
+  };
+*/
