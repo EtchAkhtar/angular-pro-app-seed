e63c6d0aed18aa22740a1b235d8faddfdd85c6d5
Etch Akhtar <etch@clearcutcoding.com>
2018-07-04 16:26:25 +0100

    Add/list/remove meals

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -8,13 +8,16 @@ import { SharedModule } from '../shared/shared.module';
 
 // containers
 import { MealsComponent } from './containers/meals/meals.component';
+import { MealComponent } from './containers/meal/meal.component';
+
+// components
+import { MealFormComponent } from './components/meal-form/meal-form.component';
 
 // routes
 export const ROUTES: Routes = [
-  {
-    path: '',
-    component: MealsComponent
-  }
+  { path: '', component: MealsComponent },
+  { path: 'new', component: MealComponent },
+  { path: ':id', component: MealComponent }
 ];
 
 @NgModule({
@@ -24,6 +27,6 @@ export const ROUTES: Routes = [
     RouterModule.forChild(ROUTES),
     SharedModule
   ],
-  declarations: [MealsComponent]
+  declarations: [MealsComponent, MealComponent, MealFormComponent]
 })
 export class MealsModule {}
--- src/health/shared/shared.module.ts
+++ src/health/shared/shared.module.ts
@@ -8,14 +8,15 @@ import { RouterModule } from '@angular/router';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 // components
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 // services
 import { MealsService } from './services/meals/meals.service';
 
 @NgModule({
   imports: [CommonModule, RouterModule, AngularFireDatabaseModule],
-  declarations: [],
-  exports: []
+  declarations: [ListItemComponent],
+  exports: [ListItemComponent]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
